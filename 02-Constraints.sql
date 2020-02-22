e977095553f3be7d2d2a8dc4fcbf58473a2de950
王權 <timowang@wangquande-MacBook-Pro.local>
2020-02-22 15:03:20 +0800

    remove unused parts in 02 constraints

--- 02-Constraints.sql
+++ 02-Constraints.sql
@@ -6,12 +6,6 @@ CREATE TABLE student (
 
 DESCRIBE student;
 
-DROP TABLE student;
-
-ALTER TABLE student ADD gpa DECIMAL(3, 2);
-
-ALTER TABLE student DROP COLUMN gpa;
-
 INSERT INTO student VALUES(1, 'Jack', 'Biology');
 INSERT INTO student VALUES(2, 'Kate', 'Sociology');
 INSERT INTO student(student_id, name, major) VALUES(3, NULL, 'Chemistry'); -- cannot be null
