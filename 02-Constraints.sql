ac646e363c50a331abf34945afe99e15781ebc85
王權 <timowang@wangquande-MacBook-Pro.local>
2020-02-22 15:05:39 +0800

    add more constraints to 02

--- 02-Constraints.sql
+++ 02-Constraints.sql
@@ -15,3 +15,20 @@ INSERT INTO student VALUES(5, 'Mike', 'Computer Science');
 SELECT * FROM student;
 
 DROP TABLE student;
+
+----------------------------
+
+CREATE TABLE student (
+	student_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20),
+    major VARCHAR(20) DEFAULT 'undecided'
+);
+
+DESCRIBE student;
+
+INSERT INTO student(name, major) VALUES('Jack', 'Biology');
+INSERT INTO student(name, major) VALUES('Kate', 'Sociology');
+
+SELECT * FROM student;
+
+DROP TABLE student;
