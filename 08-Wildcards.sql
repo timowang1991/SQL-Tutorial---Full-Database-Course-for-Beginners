148f1463e6988e8adde503d0d5340bc036fd140e
王權 <timowang@wangquande-MacBook-Pro.local>
2020-02-26 20:42:31 +0800

    add wildcards

---
+++ 08-Wildcards.sql
@@ -0,0 +1,21 @@
+-- % = any # characters, _ = one character
+
+-- Find and client's who are an LLC
+SELECT *
+FROM client
+WHERE client_name LIKE '%LLC';
+
+-- Find any branch suppliers who are in the label business
+SELECT *
+FROM branch_supplier
+WHERE supplier_name LIKE '%Label%';
+
+-- Find any employee born in February
+SELECT *
+FROM employee
+WHERE birth_day LIKE '____-02-__';
+
+-- Find any clients who are schools
+SELECT *
+FROM client
+WHERE client_name LIKE '%school%';
